e8088b0cdd7ef762e90178b255fa0138523343a9
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-05-25 19:39:45 +0700

    Test imports with `.js` extension

---
+++ src/tests/imports/imports.spec.ts
@@ -0,0 +1,35 @@
+import { testDts } from '../test-dts';
+
+describe('For imports with `.js` extension', () => {
+  it('emits type definitions', async () => {
+
+    const { files: [{ content }] } = await testDts(
+        'imports',
+        {
+          entries: { imported: {} },
+        },
+    );
+
+    expect(content).toMatchSnapshot();
+  });
+  it('emits declaration map', async () => {
+
+    const { files } = await testDts(
+        'imports',
+        {
+          entries: { imported: {} },
+          compilerOptions: { declarationMap: true },
+        },
+    );
+
+    expect(files).toHaveLength(2);
+
+    const [dts, sourceMap] = files;
+
+    expect(dts.path).toBe('index.d.ts');
+    expect(sourceMap.path).toBe('index.d.ts.map');
+
+    expect(dts.content).toMatchSnapshot('dts');
+    expect(sourceMap.content).toMatchSnapshot('source map');
+  });
+});
