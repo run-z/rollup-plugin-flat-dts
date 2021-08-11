0c83cde0187315e2bca7f485b76ec7412667b327
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-08-11 11:51:10 +0700

    Compute file paths relatively to CWD
    ...to properly evaluate `sourceRoot` of generated declaration map.

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,6 +2,7 @@
  * @packageDocumentation
  * @module Module rollup-plugin-flat-dts
  */
+import { relative, resolve } from 'path';
 import type { OutputPlugin } from 'rollup';
 import type { FlatDts } from './api';
 import { emitFlatDts } from './api';
@@ -20,7 +21,14 @@ export default function flatDtsPlugin(dtsOptions?: FlatDts.Options): OutputPlugi
 
     name: 'flat-dts',
 
-    async generateBundle(): Promise<void> {
+    async generateBundle({ dir }): Promise<void> {
+
+      let assetPath = (filePath: string): string => filePath;
+
+      if (dir != null) {
+        dtsOptions = dtsOptionsRelativeToDir(dir, dtsOptions);
+        assetPath = filePath => relative(dir, filePath);
+      }
 
       const dts = await emitFlatDts(dtsOptions);
 
@@ -31,10 +39,39 @@ export default function flatDtsPlugin(dtsOptions?: FlatDts.Options): OutputPlugi
       dts.files.forEach(({ path, content }) => {
         this.emitFile({
           type: 'asset',
-          fileName: path,
+          fileName: assetPath(path),
           source: content,
         });
       });
     },
+  };
+}
+
+function dtsOptionsRelativeToDir(dir: string, dtsOptions: FlatDts.Options = {}): FlatDts.Options {
+
+  const { file = 'index.d.ts', entries = {} } = dtsOptions;
+
+  return {
+    ...dtsOptions,
+    file: relative(process.cwd(), resolve(dir, file)),
+    entries: Object.fromEntries(
+        Object
+            .entries(entries)
+            .map(([key, dtsEntry = {}]) => [key, dtsEntryRelativeToDir(dir, dtsEntry)]),
+    ),
+  };
+}
+
+function dtsEntryRelativeToDir(dir: string, dtsEntry: FlatDts.EntryDecl = {}): FlatDts.EntryDecl {
+
+  const { file } = dtsEntry;
+
+  if (file == null) {
+    return dtsEntry;
+  }
+
+  return {
+    ...dtsEntry,
+    file: relative(process.cwd(), resolve(dir, file)),
   };
 }
